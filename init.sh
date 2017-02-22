1b6e22b51cd65bbf65678efbb6df58cd2d019e53
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2017-02-22 22:48:35 +0900

    Add .gitconfig

--- init.sh
+++ init.sh
@@ -3,5 +3,6 @@
 readonly DOTFILES_DIR=$(cd $(dirname $0) && pwd)
 
 ln --symbolic --force --verbose --target-directory=$HOME \
-  $DOTFILES_DIR/.zshrc*
+  $DOTFILES_DIR/.zshrc* \
+  $DOTFILES_DIR/.gitconfig
 
